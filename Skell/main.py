79cfaa363a179a329076dc00c715a5e43c7ddff1
Jakob Lovern <jake.lovern@gmail.com>
2020-04-04 14:38:07 -0700

    Added new possibility to main. Now we can 'generate' mountains.

--- Skell/main.py
+++ Skell/main.py
@@ -29,18 +29,21 @@ def runDD():
         #ignore messages from myself
         if message.author == client.user:
             return
-        
-        if message.content == "$Shastabot fact get":
-            await message.channel.send("Did you know: "+GetShastaFact())
-        elif message.content.startswith("$Shastabot fact add"):
-            AddShastaFact(message.content[18:])
-            await message.channel.send("Fact added: "+message.content[18:])
-        elif message.content == "$Shastabot test":
-            await message.channel.send(config["DISCORD"]["test_phrase"])
-        elif message.content.startswith("$Shastabot "):
-            out = textAdventure.readCommand(message.content[10:])
-            for line in out:
-                await message.channel.send(line)
+        if message.content.startswith("$shastabot "):
+            message.content = message.content[10:]
+            if message.content == "fact get":
+                await message.channel.send("Did you know: "+GetShastaFact())
+            elif message.content.startswith("fact add"):
+                AddShastaFact(message.content[18:])
+                await message.channel.send("Fact added: "+message.content[18:])
+            elif message.content == "test":
+                await message.channel.send(config["DISCORD"]["test_phrase"])
+            elif message.content == "generate":
+                await message.channel.send(file="MtShasta_aerial.JPG")
+            elif message.content.startswith("$Shastabot "):
+                out = textAdventure.readCommand(message.content[10:])
+                for line in out:
+                    await message.channel.send(line)
     client.run(config["DISCORD"]["discord_token"])
     
 if __name__ == "__main__":
