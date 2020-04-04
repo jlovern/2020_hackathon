4e503ca2d3f5a5e67718fbe4c6e06ef1c1a2c7ac
Jakob Lovern <jake.lovern@gmail.com>
2020-04-04 14:15:15 -0700

    Utility commit so I can transfer files to server

--- Skell/main.py
+++ Skell/main.py
@@ -7,9 +7,10 @@ config.read("config.ini")
 
 def GetShastaFact():
     out = ""
-    with open(config['DEFAULT']['shasta_facts'],"r") as shasta_facts:
-        lines = shasta_facts.readLines()
-        out = lines[randint(0,len(lines)-1)]
+    shasta_facts = open(config['DEFAULT']['shasta_facts'],"r")
+    lines = shasta_facts.readLines()
+    out = lines[randint(0,len(lines)-1)]
+    shasta_facts.close()
     return out
 
 def AddShastaFact(fact):
@@ -32,20 +33,15 @@ def runDD():
         if message.content == "$Shastabot fact get":
             await message.channel.send("Did you know: "+GetShastaFact())
         elif message.content.startswith("$Shastabot fact add"):
-            AddShastaFact(message.content[19:])
-            await message.channel.send("Fact added: "+message.content[19:])
+            AddShastaFact(message.content[18:])
+            await message.channel.send("Fact added: "+message.content[18:])
         elif message.content == "$Shastabot test":
             await message.channel.send(config["DISCORD"]["test_phrase"])
         elif message.content.startswith("$Shastabot "):
-            out = textAdventure.readCommand(message.content[11:])
+            out = textAdventure.readCommand(message.content[10:])
             for line in out:
                 await message.channel.send(line)
     client.run(config["DISCORD"]["discord_token"])
     
-def main():
-    runDD()
-        
-
-    
 if __name__ == "__main__":
-    main()
+    runDD()
