a61a8d9d4cc1af9214de45ad79ad88b8bfd0b3e4
Jakob Lovern <jake.lovern@gmail.com>
2020-04-04 12:24:27 -0700

    Initial Commit

---
+++ Skell/main.py
@@ -0,0 +1,7 @@
+def intro_text():
+    print("There is something special about the mountains.")
+    print("Maybe it's just that I was raised there.")
+    print("")
+    
+if __name__=="__main__":
+    intro_text()
