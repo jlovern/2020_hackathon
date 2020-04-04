80bfbc76dfdb072490df17fbfa5a7e9090d64cc7
Jakob Lovern <jake.lovern@gmail.com>
2020-04-04 14:09:45 -0700

    Full commit this time. I'm getting it!

--- Skell/main.py
+++ Skell/main.py
@@ -1,7 +1,51 @@
-def intro_text():
-    print("There is something special about the mountains.")
-    print("Maybe it's just that I was raised there.")
-    print("")
+import configparser
+import discord as d
+from random import randint
+import textAdventure
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+def GetShastaFact():
+    out = ""
+    with open(config['DEFAULT']['shasta_facts'],"r") as shasta_facts:
+        lines = shasta_facts.readLines()
+        out = lines[randint(0,len(lines)-1)]
+    return out
+
+def AddShastaFact(fact):
+    with open(config['DEFAULT']['shasta_facts'],"a") as shasta_facts:
+        shasta_facts.write(fact)
     
-if __name__=="__main__":
-    intro_text()
+def runDD():
+    client = d.Client()
+
+    @client.event
+    async def on_ready():
+        print('We have logged in as {0.user}'.format(client))
+    
+    @client.event
+    async def on_message(message):
+        #ignore messages from myself
+        if message.author == client.user:
+            return
+        
+        if message.content == "$Shastabot fact get":
+            await message.channel.send("Did you know: "+GetShastaFact())
+        elif message.content.startswith("$Shastabot fact add"):
+            AddShastaFact(message.content[19:])
+            await message.channel.send("Fact added: "+message.content[19:])
+        elif message.content == "$Shastabot test":
+            await message.channel.send(config["DISCORD"]["test_phrase"])
+        elif message.content.startswith("$Shastabot "):
+            out = textAdventure.readCommand(message.content[11:])
+            for line in out:
+                await message.channel.send(line)
+    client.run(config["DISCORD"]["discord_token"])
+    
+def main():
+    runDD()
+        
+
+    
+if __name__ == "__main__":
+    main()
